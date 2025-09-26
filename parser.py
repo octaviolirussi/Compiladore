cc872ab2ced5fc0c0956468c933132b90fd3382d
OctavioLirussi <octaviolirussi@gmail.com>
2025-09-26 19:43:43 -0300

    Parser

--- parser.py
+++ parser.py
@@ -23,20 +23,31 @@ class MyParser(Parser):
     def statement_list(self, p):
         return [p.statement]
 
-    # === SENTENCIAS ===
-    # @_('CONST_INT ";"')
-    # def statement(self, p):
-    #     return ('num_stmt', p.CONST_INT)
+    # === SENTENCIAS =============================================================================================================================
 
     @_('expr ";"')
     def statement(self, p):
-        return ('expr_stmt', p.expr)
+        return (f"Linea {p.lineno} --> expr_stmt:", p.expr)
 
     @_('ID "=" expr ";"')
     def statement(self, p):
-        return ('assign', p.ID, p.expr)
+        return (f"Linea: {p.lineno} --> assign", p.ID, p.expr)
+    
+    @_('INT id_list ";"')
+    def statement(self, p):
+        return (f"Linea: {p.lineno} --> declaration:", p.INT, p.id_list)
+    
+    @_('FLOAT id_list ";"')
+    def statement(self, p):
+        return (f"Linea: {p.lineno} --> declaration:", p.FLOAT, p.id_list)
+    
+    # Regla recursiva para lista de IDs
+    @_('id_list "," ID')
+    def id_list(self, p):
+        return p.id_list + [p.ID]
+    
 
-    # === EXPRESIONES ===
+    # === EXPRESIONES ==========================================================================================================================
     @_('expr "+" expr')
     def expr(self, p):
         return ('suma', p.expr0, p.expr1)
@@ -52,11 +63,27 @@ class MyParser(Parser):
     @_('expr "/" expr')
     def expr(self, p):
         return ('division', p.expr0, p.expr1)
-
-    @_('CONST_INT')
-    def expr(self, p):
-        return ('num', p.CONST_INT)
-
+    
+    @_('ID')
+    def id_list(self, p):
+        return [p.ID]
+    
     @_('ID')
     def expr(self, p):
         return ('var', p.ID)
+
+    @_('CONST_INT')
+    def expr(self, p):
+        return ('num_int', p.CONST_INT)
+    
+    @_('CONST_FLOAT')
+    def expr(self, p):
+        return ('num_float', p.CONST_FLOAT)
+    
+    @_('INT')
+    def expr(self, p):
+        return ('int',p.INT)
+    
+    @_('FLOAT')
+    def expr(self, p):
+        return ('float',p.FLOAT)
